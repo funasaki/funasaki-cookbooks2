30d3bc98cee9522ecf8b13fa0de785f59d4fece3
EC2 Default User <ec2-user@web1.localdomain>
2016-01-12 08:11:36 +0000

    5th

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,4 +1,7 @@
 Chef::Log.info("before_migrate.rb executing")
+application = new_resource.params[:app]
+deploy = new_resource.params[:deploy_data]
+Chef::Log.info("USER_ID: #{node[:deploy]['app1'][:environment_variables][:USER_ID]}")
 
 file "/home/ec2-user/tmp/hello-call-back1.txt" do
   action :create
